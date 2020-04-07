570cda9e65f24f8462a6603838189dd357d5973d
Amy Bartels <akunrau@gmail.com>
2020-04-07 22:30:07 +0000

    checked for and fixed linting

--- timer1.js
+++ timer1.js
@@ -1,13 +1,13 @@
-const cmdLineArgs = process.argv.slice(2)
+const cmdLineArgs = process.argv.slice(2);
 
 
 for (const index in cmdLineArgs) {
   if (!isNaN(cmdLineArgs[index]) && (cmdLineArgs[index] > 0)) {
     setTimeout(() => {
-      console.log(cmdLineArgs[index])
+      // console.log(cmdLineArgs[index]);
       process.stdout.write('*BEEP*\n');
-    }, cmdLineArgs[index] * 1000)
-  }   
+    }, cmdLineArgs[index] * 1000);
+  }
 }
 
-// 
+//
