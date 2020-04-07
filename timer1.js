114326010ab64dcc423e31d98d168f58c246bc90
Amy Bartels <akunrau@gmail.com>
2020-04-07 22:27:44 +0000

    created a simple timer to 'beep' for every smout of seconds given in the cmd line. Will not accept NaN or negatives.

---
+++ timer1.js
@@ -0,0 +1,13 @@
+const cmdLineArgs = process.argv.slice(2)
+
+
+for (const index in cmdLineArgs) {
+  if (!isNaN(cmdLineArgs[index]) && (cmdLineArgs[index] > 0)) {
+    setTimeout(() => {
+      console.log(cmdLineArgs[index])
+      process.stdout.write('*BEEP*\n');
+    }, cmdLineArgs[index] * 1000)
+  }   
+}
+
+// 
